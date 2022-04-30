f14e46cff0a40093688d2bc57e75a0016bab09c6
alegomezs <agomezsantillan11@gmail.com>
2022-04-30 02:05:07 -0300

    Component importation renamed from Body to Main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.scss";
 import Header from "./components/Header";
-import Body from "./components/Main";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Main />
       <Footer />
     </div>
   );
